dfe0ed18a6d3858d7a6b324372a882305a887674
Peng Su <peng.su@woolworths.co.nz>
2024-10-10 09:10:21 +1300

    navigation set up

---
+++ apps/host/rspack.config.prod.ts
@@ -0,0 +1,51 @@
+import { composePlugins, withNx, withReact } from '@nx/rspack';
+import {
+  withModuleFederation,
+  ModuleFederationConfig,
+} from '@nx/rspack/module-federation';
+import path from 'path';
+
+import baseConfig from './module-federation.config';
+
+const prodConfig: ModuleFederationConfig = {
+  ...baseConfig,
+  /*
+   * Remote overrides for production.
+   * Each entry is a pair of a unique name and the URL where it is deployed.
+   *
+   * e.g.
+   * remotes: [
+   *   ['app1', 'http://app1.example.com'],
+   *   ['app2', 'http://app2.example.com'],
+   * ]
+   *
+   * You can also use a full path to the remoteEntry.js file if desired.
+   *
+   * remotes: [
+   *   ['app1', 'http://example.com/path/to/app1/remoteEntry.js'],
+   *   ['app2', 'http://example.com/path/to/app2/remoteEntry.js'],
+   * ]
+   */
+  remotes: [
+    // ['shop', 'http://localhost:4201/shop'],
+    // ['cart', 'http://localhost:4202/cart'],
+    // ['about', 'http://localhost:4203/about'],
+    ['shop', 'http://localhost:3000/shop'],
+    ['cart', 'http://localhost:3000/cart'],
+    ['about', 'http://localhost:3000/about'],
+  ],
+  // runtimePlugins:[path.resolve(__dirname,'shared-strategy.ts')],
+  disableNxRuntimeLibraryControlPlugin: false
+};
+
+// Nx plugins for rspack to build config object from Nx options and context.
+/**
+ * DTS Plugin is disabled in Nx Workspaces as Nx already provides Typing support for Module Federation
+ * The DTS Plugin can be enabled by setting dts: true
+ * Learn more about the DTS Plugin here: https://module-federation.io/configure/dts.html
+ */
+export default composePlugins(
+  withNx(),
+  withReact(),
+  withModuleFederation(prodConfig, { dts: false })
+);
