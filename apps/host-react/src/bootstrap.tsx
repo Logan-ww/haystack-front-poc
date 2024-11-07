4a0cd3cbc808a87020cb776de2688aef25450442
Peng Su <peng.su@woolworths.co.nz>
2024-11-07 18:02:56 +1300

    seo helmet async

--- apps/host-react/src/bootstrap.tsx
+++ apps/host-react/src/bootstrap.tsx
@@ -1,14 +1,24 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 import App from './app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const helmetContext = {};
+
 root.render(
   <BrowserRouter>
+    <HelmetProvider context={helmetContext}>
+      <Helmet>
+        <title>Hello World</title>
+        <link rel="canonical" href="https://www.tacobell.com/" />
+      </Helmet>
     <App />
+    </HelmetProvider>
   </BrowserRouter>
 );
--- apps/remote-content-pages/src/app/app.tsx
+++ apps/remote-content-pages/src/app/app.tsx
@@ -1,12 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 import { Settings } from './components/Settings';
+import { Helmet } from 'react-helmet-async';
 
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
     <div>
+      <Helmet>
+        <title>Content Page</title>
+        <link rel="canonical" href="https://www.otherstuff.com/" />
+      </Helmet>
       <NxWelcome title="Content Page" />
       <Settings />
     </div>
