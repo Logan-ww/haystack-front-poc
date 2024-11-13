05b6ea693fe6947c48720fcad59be11589f9fd33
Said Kholov <skholov2@gmail.com>
2024-11-14 10:35:06 +1300

    add acoverage

--- apps/remote-home/.storybook/main.ts
+++ apps/remote-home/.storybook/main.ts
@@ -9,7 +9,8 @@ const config: StorybookConfig = {
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/addon-a11y'
+    '@storybook/addon-a11y',
+    '@storybook/addon-coverage',
   ],
   framework: {
     name: '@storybook/react-vite',
